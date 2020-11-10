ca3710fea01d7a59394c858ee73759b0b322466d
dxcjc <2462824378@qq.com>
2020-11-10 17:11:08 +0800

    fix:添加数据库

--- router/infoRouter.js
+++
@@ -1,9 +0,0 @@
-const infoCtrl = require('../controller/infoCtrl')
-const router = require('../middleware/router')
-
-//infoCtrl.getList
-router.get('/info/list',infoCtrl.getList)
-router.get('/info/getInfoByUid',infoCtrl.getInfoByUid)
-router.post('/info/edit', infoCtrl.edit)
-
-module.exports = router;
